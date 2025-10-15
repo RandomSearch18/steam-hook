6caa44ca1fad1ee4830f8635f7af8941593eb687
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-10-15 22:47:19 +0100

    Somewhat put it all together (no Slacking yet)

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
+from time import sleep
 from dotenv import load_dotenv
 from os import getenv
 
 from steam_client import Steam
 
 
-def main():
-    steam = Steam(api_key=getenv("STEAM_API_KEY"))
+def handle_newly_earned_achievement(steam_id: int, app_id: int, achievement_id: str):
+    print(
+        f"Player {steam_id} has just earned achievement {achievement_id} in game {app_id}"
+    )
 
-    game = 504230
-    user = getenv("STEAM_USER_ID")
+
+def main():
+    steam_api_key = getenv("STEAM_API_KEY")
+    if not steam_api_key:
+        raise ValueError("No STEAM_API_KEY found in environment variables")
+    steam = Steam(api_key=steam_api_key)
+    steam.handle_newly_earned_achievement = handle_newly_earned_achievement
+
     # game_schema = steam.fetch_game_schema(appid=game)
     # achievements = game_schema["game"]["availableGameStats"]["achievements"]
     # print(game_schema)
@@ -17,8 +26,10 @@ def main():
     # print(user_achievements)
     # global_achievements = steam.fetch_global_achievement_percentages(app_id=game)
     # print(global_achievements)
-    x = steam.update_player_achievement_matrix(steam_id=int(user))
-    print(x)
+    while True:
+        user_id = int(getenv("STEAM_USER_ID"))
+        steam.update_player_achievement_matrix(steam_id=user_id)
+        sleep(10)
 
 
 if __name__ == "__main__":
