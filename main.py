481ac852f7218f1fc79a5244458213f95dea1a6d
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-10-15 09:30:35 +0100

    Add .env and dotenv

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+from steam.client import SteamClient
+from steam.enums.emsg import EMsg
+from dotenv import load_dotenv
+
+
 def main():
-    print("Hello from steam-hook!")
+    client = SteamClient()
+
+    @client.on(EMsg.ClientVACBanStatus)
+    def print_vac_status(msg):
+        print("Number of VAC Bans: %s" % msg.body.numBans)
+
+    client.cli_login()
+
+    print("Logged on as: %s" % client.user.name)
+    print("Community profile: %s" % client.steam_id.community_url)
+    print("Last logon: %s" % client.user.last_logon)
+    print("Last logoff: %s" % client.user.last_logoff)
+    print("Number of friends: %d" % len(client.friends))
+
+    client.logout()
 
 
 if __name__ == "__main__":
+    load_dotenv()
     main()
