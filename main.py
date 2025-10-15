53b732e7809855a0f5c7875d4034c81e5950c787
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-10-15 21:01:27 +0100

    ooo yes we're definitely getting somewhere

--- main.py
+++ main.py
@@ -6,8 +6,19 @@ from os import getenv
 def main():
     steam = WebAPI(key=getenv("STEAM_API_KEY"))
 
-    print(steam.ISteamWebAPIUtil.GetServerInfo())
-    print(steam.call("ISteamWebAPIUtil.GetServerInfo"))
+    game = 504230
+    user = 
+    game_schema = steam.call("ISteamUserStats.GetSchemaForGame", appid=game)
+    achievements = game_schema["game"]["availableGameStats"]["achievements"]
+    print(game_schema)
+    user_achievements = steam.call(
+        "ISteamUserStats.GetPlayerAchievements", appid=game, steamid=user
+    )
+    print(user_achievements)
+    global_achievements = steam.call(
+        "ISteamUserStats.GetGlobalAchievementPercentagesForApp", gameid=game
+    )
+    print(global_achievements)
 
 
 if __name__ == "__main__":
