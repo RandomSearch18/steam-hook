ef3760a38434a110bd70f1b062eb5b62855fdc1d
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-10-15 09:33:43 +0100

    Use the web API instead

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
-from steam.client import SteamClient
-from steam.enums.emsg import EMsg
+from steam.webapi import WebAPI
 from dotenv import load_dotenv
+from os import getenv
 
 
 def main():
-    client = SteamClient()
+    steam = WebAPI(key=getenv("STEAM_API_KEY"))
 
-    @client.on(EMsg.ClientVACBanStatus)
-    def print_vac_status(msg):
-        print("Number of VAC Bans: %s" % msg.body.numBans)
-
-    client.cli_login()
-
-    print("Logged on as: %s" % client.user.name)
-    print("Community profile: %s" % client.steam_id.community_url)
-    print("Last logon: %s" % client.user.last_logon)
-    print("Last logoff: %s" % client.user.last_logoff)
-    print("Number of friends: %d" % len(client.friends))
-
-    client.logout()
+    print(steam.ISteamWebAPIUtil.GetServerInfo())
+    print(steam.call("ISteamWebAPIUtil.GetServerInfo"))
 
 
 if __name__ == "__main__":
