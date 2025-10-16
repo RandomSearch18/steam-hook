df8952aa8a7b73d741fd97aa9213f241b332e40f
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-10-16 09:19:23 +0000

    refactor: Deduplicate initialisation logic

--- steam_hook/__init__.py
+++ steam_hook/__init__.py
@@ -1,11 +1,5 @@
-# Entrypoint when using `flask --app steam_hook run`
-from threading import Thread
-from dotenv import load_dotenv
-from steam_hook.steam_tracker import steam_tracker
-from steam_hook.web_ui import app
-
+from steam_hook.main import initialize_app
 
 def create_app(test_config=None):
-    load_dotenv()
-    Thread(target=steam_tracker, daemon=True).start()
+    app = initialize_app()
     return app
--- steam_hook/main.py
+++ steam_hook/main.py
@@ -1,13 +1,16 @@
-# Entrypoint when not using the Flask CLI
 from threading import Thread
 from dotenv import load_dotenv
 from steam_hook.steam_tracker import steam_tracker
 from steam_hook.web_ui import app
 
-
-def main():
+def initialize_app():
     load_dotenv()
     Thread(target=steam_tracker, daemon=True).start()
+    return app
+
+def main():
+    # Entrypoint when not using the Flask CLI
+    app = initialize_app()
     app.run(host="0.0.0.0", port=5000)
 
 
